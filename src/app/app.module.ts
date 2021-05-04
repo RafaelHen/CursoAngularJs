ad3b078feb74a9140ff1a8c402d500dea873eac8
RafaelHen <rafaelvictor368@hotmail.com>
2021-05-04 12:38:30 -0300

    Aula 60

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { routing } from './app.routing'
 import { FormsModule } from '@angular/forms';
+import { CursosService } from './cursos/cursos.service';
+//import { routing } from './app.routing'
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+//import { CursosComponent } from './cursos/cursos.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+import { AppRoutingModule } from './app.routing.module';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent,
+    //CursosComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+   //CursoDetalheComponent,
+   // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    AppRoutingModule,
+    CursosModule
+    //routing
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
